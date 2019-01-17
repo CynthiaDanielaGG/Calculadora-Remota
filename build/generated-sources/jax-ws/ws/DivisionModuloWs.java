26568f449ea8c580723248dda63c1c1a408d7826
CynthiaDanielaGG <danieladevwoman@gmail.com>
2019-01-17 15:33:15 -0600

    first commit calculator remote in java

---
+++ build/generated-sources/jax-ws/ws/DivisionModuloWs.java
@@ -0,0 +1,71 @@
+
+package ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para division_modulo_ws complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="division_modulo_ws">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="arg0" type="{http://www.w3.org/2001/XMLSchema}float"/>
+ *         &lt;element name="arg1" type="{http://www.w3.org/2001/XMLSchema}float"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "division_modulo_ws", propOrder = {
+    "arg0",
+    "arg1"
+})
+public class DivisionModuloWs {
+
+    protected float arg0;
+    protected float arg1;
+
+    /**
+     * Obtiene el valor de la propiedad arg0.
+     * 
+     */
+    public float getArg0() {
+        return arg0;
+    }
+
+    /**
+     * Define el valor de la propiedad arg0.
+     * 
+     */
+    public void setArg0(float value) {
+        this.arg0 = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad arg1.
+     * 
+     */
+    public float getArg1() {
+        return arg1;
+    }
+
+    /**
+     * Define el valor de la propiedad arg1.
+     * 
+     */
+    public void setArg1(float value) {
+        this.arg1 = value;
+    }
+
+}
---
+++ build/generated/jax-wsCache/OperacionDosService/ws/ObjectFactory.java
@@ -0,0 +1,431 @@
+
+package ws;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the ws package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _SinWsResponse_QNAME = new QName("http://ws/", "sin_wsResponse");
+    private final static QName _DivisionModuloWs_QNAME = new QName("http://ws/", "division_modulo_ws");
+    private final static QName _PotenciaWsResponse_QNAME = new QName("http://ws/", "potencia_wsResponse");
+    private final static QName _RaizCuadradaWsResponse_QNAME = new QName("http://ws/", "raiz_cuadrada_wsResponse");
+    private final static QName _SumaWs_QNAME = new QName("http://ws/", "suma_ws");
+    private final static QName _CosWsResponse_QNAME = new QName("http://ws/", "cos_wsResponse");
+    private final static QName _RestaWs_QNAME = new QName("http://ws/", "resta_ws");
+    private final static QName _TanWs_QNAME = new QName("http://ws/", "tan_ws");
+    private final static QName _DivisionModuloWsResponse_QNAME = new QName("http://ws/", "division_modulo_wsResponse");
+    private final static QName _RestaWsResponse_QNAME = new QName("http://ws/", "resta_wsResponse");
+    private final static QName _MultiplicacionWs_QNAME = new QName("http://ws/", "multiplicacion_ws");
+    private final static QName _DivisionWs_QNAME = new QName("http://ws/", "division_ws");
+    private final static QName _DivisionWsResponse_QNAME = new QName("http://ws/", "division_wsResponse");
+    private final static QName _MultiplicacionWsResponse_QNAME = new QName("http://ws/", "multiplicacion_wsResponse");
+    private final static QName _PotenciaWs_QNAME = new QName("http://ws/", "potencia_ws");
+    private final static QName _RaizCubicaWsResponse_QNAME = new QName("http://ws/", "raiz_cubica_wsResponse");
+    private final static QName _TanWsResponse_QNAME = new QName("http://ws/", "tan_wsResponse");
+    private final static QName _RaizCubicaWs_QNAME = new QName("http://ws/", "raiz_cubica_ws");
+    private final static QName _CosWs_QNAME = new QName("http://ws/", "cos_ws");
+    private final static QName _RaizCuadradaWs_QNAME = new QName("http://ws/", "raiz_cuadrada_ws");
+    private final static QName _SinWs_QNAME = new QName("http://ws/", "sin_ws");
+    private final static QName _SumaWsResponse_QNAME = new QName("http://ws/", "suma_wsResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: ws
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link SinWsResponse }
+     * 
+     */
+    public SinWsResponse createSinWsResponse() {
+        return new SinWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link DivisionModuloWs }
+     * 
+     */
+    public DivisionModuloWs createDivisionModuloWs() {
+        return new DivisionModuloWs();
+    }
+
+    /**
+     * Create an instance of {@link PotenciaWsResponse }
+     * 
+     */
+    public PotenciaWsResponse createPotenciaWsResponse() {
+        return new PotenciaWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link RaizCuadradaWsResponse }
+     * 
+     */
+    public RaizCuadradaWsResponse createRaizCuadradaWsResponse() {
+        return new RaizCuadradaWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link SumaWs }
+     * 
+     */
+    public SumaWs createSumaWs() {
+        return new SumaWs();
+    }
+
+    /**
+     * Create an instance of {@link CosWsResponse }
+     * 
+     */
+    public CosWsResponse createCosWsResponse() {
+        return new CosWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link RestaWs }
+     * 
+     */
+    public RestaWs createRestaWs() {
+        return new RestaWs();
+    }
+
+    /**
+     * Create an instance of {@link TanWs }
+     * 
+     */
+    public TanWs createTanWs() {
+        return new TanWs();
+    }
+
+    /**
+     * Create an instance of {@link DivisionModuloWsResponse }
+     * 
+     */
+    public DivisionModuloWsResponse createDivisionModuloWsResponse() {
+        return new DivisionModuloWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link RestaWsResponse }
+     * 
+     */
+    public RestaWsResponse createRestaWsResponse() {
+        return new RestaWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link MultiplicacionWs }
+     * 
+     */
+    public MultiplicacionWs createMultiplicacionWs() {
+        return new MultiplicacionWs();
+    }
+
+    /**
+     * Create an instance of {@link DivisionWs }
+     * 
+     */
+    public DivisionWs createDivisionWs() {
+        return new DivisionWs();
+    }
+
+    /**
+     * Create an instance of {@link DivisionWsResponse }
+     * 
+     */
+    public DivisionWsResponse createDivisionWsResponse() {
+        return new DivisionWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link MultiplicacionWsResponse }
+     * 
+     */
+    public MultiplicacionWsResponse createMultiplicacionWsResponse() {
+        return new MultiplicacionWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link PotenciaWs }
+     * 
+     */
+    public PotenciaWs createPotenciaWs() {
+        return new PotenciaWs();
+    }
+
+    /**
+     * Create an instance of {@link RaizCubicaWsResponse }
+     * 
+     */
+    public RaizCubicaWsResponse createRaizCubicaWsResponse() {
+        return new RaizCubicaWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link TanWsResponse }
+     * 
+     */
+    public TanWsResponse createTanWsResponse() {
+        return new TanWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link RaizCubicaWs }
+     * 
+     */
+    public RaizCubicaWs createRaizCubicaWs() {
+        return new RaizCubicaWs();
+    }
+
+    /**
+     * Create an instance of {@link CosWs }
+     * 
+     */
+    public CosWs createCosWs() {
+        return new CosWs();
+    }
+
+    /**
+     * Create an instance of {@link RaizCuadradaWs }
+     * 
+     */
+    public RaizCuadradaWs createRaizCuadradaWs() {
+        return new RaizCuadradaWs();
+    }
+
+    /**
+     * Create an instance of {@link SinWs }
+     * 
+     */
+    public SinWs createSinWs() {
+        return new SinWs();
+    }
+
+    /**
+     * Create an instance of {@link SumaWsResponse }
+     * 
+     */
+    public SumaWsResponse createSumaWsResponse() {
+        return new SumaWsResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SinWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "sin_wsResponse")
+    public JAXBElement<SinWsResponse> createSinWsResponse(SinWsResponse value) {
+        return new JAXBElement<SinWsResponse>(_SinWsResponse_QNAME, SinWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DivisionModuloWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "division_modulo_ws")
+    public JAXBElement<DivisionModuloWs> createDivisionModuloWs(DivisionModuloWs value) {
+        return new JAXBElement<DivisionModuloWs>(_DivisionModuloWs_QNAME, DivisionModuloWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link PotenciaWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "potencia_wsResponse")
+    public JAXBElement<PotenciaWsResponse> createPotenciaWsResponse(PotenciaWsResponse value) {
+        return new JAXBElement<PotenciaWsResponse>(_PotenciaWsResponse_QNAME, PotenciaWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RaizCuadradaWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "raiz_cuadrada_wsResponse")
+    public JAXBElement<RaizCuadradaWsResponse> createRaizCuadradaWsResponse(RaizCuadradaWsResponse value) {
+        return new JAXBElement<RaizCuadradaWsResponse>(_RaizCuadradaWsResponse_QNAME, RaizCuadradaWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SumaWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "suma_ws")
+    public JAXBElement<SumaWs> createSumaWs(SumaWs value) {
+        return new JAXBElement<SumaWs>(_SumaWs_QNAME, SumaWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CosWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "cos_wsResponse")
+    public JAXBElement<CosWsResponse> createCosWsResponse(CosWsResponse value) {
+        return new JAXBElement<CosWsResponse>(_CosWsResponse_QNAME, CosWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RestaWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "resta_ws")
+    public JAXBElement<RestaWs> createRestaWs(RestaWs value) {
+        return new JAXBElement<RestaWs>(_RestaWs_QNAME, RestaWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link TanWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "tan_ws")
+    public JAXBElement<TanWs> createTanWs(TanWs value) {
+        return new JAXBElement<TanWs>(_TanWs_QNAME, TanWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DivisionModuloWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "division_modulo_wsResponse")
+    public JAXBElement<DivisionModuloWsResponse> createDivisionModuloWsResponse(DivisionModuloWsResponse value) {
+        return new JAXBElement<DivisionModuloWsResponse>(_DivisionModuloWsResponse_QNAME, DivisionModuloWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RestaWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "resta_wsResponse")
+    public JAXBElement<RestaWsResponse> createRestaWsResponse(RestaWsResponse value) {
+        return new JAXBElement<RestaWsResponse>(_RestaWsResponse_QNAME, RestaWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MultiplicacionWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "multiplicacion_ws")
+    public JAXBElement<MultiplicacionWs> createMultiplicacionWs(MultiplicacionWs value) {
+        return new JAXBElement<MultiplicacionWs>(_MultiplicacionWs_QNAME, MultiplicacionWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DivisionWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "division_ws")
+    public JAXBElement<DivisionWs> createDivisionWs(DivisionWs value) {
+        return new JAXBElement<DivisionWs>(_DivisionWs_QNAME, DivisionWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DivisionWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "division_wsResponse")
+    public JAXBElement<DivisionWsResponse> createDivisionWsResponse(DivisionWsResponse value) {
+        return new JAXBElement<DivisionWsResponse>(_DivisionWsResponse_QNAME, DivisionWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MultiplicacionWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "multiplicacion_wsResponse")
+    public JAXBElement<MultiplicacionWsResponse> createMultiplicacionWsResponse(MultiplicacionWsResponse value) {
+        return new JAXBElement<MultiplicacionWsResponse>(_MultiplicacionWsResponse_QNAME, MultiplicacionWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link PotenciaWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "potencia_ws")
+    public JAXBElement<PotenciaWs> createPotenciaWs(PotenciaWs value) {
+        return new JAXBElement<PotenciaWs>(_PotenciaWs_QNAME, PotenciaWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RaizCubicaWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "raiz_cubica_wsResponse")
+    public JAXBElement<RaizCubicaWsResponse> createRaizCubicaWsResponse(RaizCubicaWsResponse value) {
+        return new JAXBElement<RaizCubicaWsResponse>(_RaizCubicaWsResponse_QNAME, RaizCubicaWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link TanWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "tan_wsResponse")
+    public JAXBElement<TanWsResponse> createTanWsResponse(TanWsResponse value) {
+        return new JAXBElement<TanWsResponse>(_TanWsResponse_QNAME, TanWsResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RaizCubicaWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "raiz_cubica_ws")
+    public JAXBElement<RaizCubicaWs> createRaizCubicaWs(RaizCubicaWs value) {
+        return new JAXBElement<RaizCubicaWs>(_RaizCubicaWs_QNAME, RaizCubicaWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CosWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "cos_ws")
+    public JAXBElement<CosWs> createCosWs(CosWs value) {
+        return new JAXBElement<CosWs>(_CosWs_QNAME, CosWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RaizCuadradaWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "raiz_cuadrada_ws")
+    public JAXBElement<RaizCuadradaWs> createRaizCuadradaWs(RaizCuadradaWs value) {
+        return new JAXBElement<RaizCuadradaWs>(_RaizCuadradaWs_QNAME, RaizCuadradaWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SinWs }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "sin_ws")
+    public JAXBElement<SinWs> createSinWs(SinWs value) {
+        return new JAXBElement<SinWs>(_SinWs_QNAME, SinWs.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SumaWsResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws/", name = "suma_wsResponse")
+    public JAXBElement<SumaWsResponse> createSumaWsResponse(SumaWsResponse value) {
+        return new JAXBElement<SumaWsResponse>(_SumaWsResponse_QNAME, SumaWsResponse.class, null, value);
+    }
+
+}
